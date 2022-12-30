128742daf2034fd49d9d7e6d60f4a7801c8b631e
miillaadddddd <milad7212@gmail.com>
2022-12-30 08:52:57 +0330

    Add post request

--- src/bookmarks/bookmarks.controller.ts
+++ src/bookmarks/bookmarks.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Bookmark } from './bookmark.model';
+import { BookmarksService } from './bookmarks.service';
 
 @Controller('bookmarks')
-export class BookmarksController {}
+export class BookmarksController {
+  constructor(private bookmarksService: BookmarksService) {}
+
+  @Get()
+  findAll(): Bookmark[] {
+    return this.bookmarksService.findAll();
+  }
+  @Post()
+  createBookmark(@Body('url') url, @Body('description') description) {
+    return this.bookmarksService.createBookmark(url, description);
+  }
+}
---
+++ src/bookmarks/bookmarks.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { Bookmark } from './bookmark.model';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class BookmarksService {
+  private bookmarks: Bookmark[] = [];
+
+  findAll(): Bookmark[] {
+    return this.bookmarks;
+  }
+
+  createBookmark(url: string, description: string): Bookmark {
+    const bookmark: Bookmark = {
+      id: uuid(),
+      url,
+      description,
+    };
+    this.bookmarks.push(bookmark);
+
+    return bookmark;
+  }
+}
